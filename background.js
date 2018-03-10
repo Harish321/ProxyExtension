e1a8a2dd29d14c20bec6d256d78e6491ddbf92d4
Harish321 <harish.d14@iiits.in>
2018-03-10 11:39:41 +0530

    On/Off functionality

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 var button = document.getElementById('proxybutton');
-button.onclick = proxy();
-function proxy(){
+button.onclick = proxy(button.value);
+function proxy(value){
     console.log('Entered');
     var config = {
     mode: "fixed_servers",
@@ -14,8 +14,19 @@ function proxy(){
       bypassList: ["foobar.com"]
     }
   };
+  if(value == 1){
+    button.value = 2;
+    console.log('Changed');
   chrome.proxy.settings.set(
       {value: config, scope: 'regular'},
       function() {});
+  }
+  else{
+      button.value =1;
+      console.log('Cleared');
+    chrome.proxy.settings.clear(
+        {scope: 'regular'},
+        function() {});
+  }
 }
 }
