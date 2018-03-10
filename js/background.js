8669d622051b6f97f0ac3ce94e868a87380a444b
Harish Dhulipalla <harish.d14@iiits.in>
2018-03-10 15:38:26 +0530

    styles are add

--- js/background.js
+++ js/background.js
@@ -5,6 +5,12 @@ window.onload = function(){
                 if(!texts!=null){
                 var button = document.getElementById('proxybutton');
                 button.textContent = texts.text;
+                if(texts.text=="On"){
+                    document.getElementById('main_body').style.backgroundColor="green";
+                }
+                else{
+                    document.getElementById('main_body').style.backgroundColor="red";
+                }
                 }
             })
             button.value = results.value;
@@ -13,12 +19,12 @@ window.onload = function(){
 }
 
 var button = document.getElementById('proxybutton');
-
+var body = document.getElementById('main_body');
 button.onclick = function(){
     var config = {
     mode: "fixed_servers",
     rules: {
-    proxyForHttp: {
+        singleProxy: {
         scheme: "socks5",
         host: "127.0.0.1",
         port: 8910
@@ -29,14 +35,16 @@ button.onclick = function(){
   if(button.value == 1){
     button.value =2;
     button.textContent = "Off";
+    body.style.backgroundColor = "red";
     chrome.storage.sync.set({'value':2,'text':"Off"},function(){});
-  chrome.proxy.settings.set(
-      {value: config, scope: 'regular'},
-      function() {});
+    chrome.proxy.settings.set(
+    {value: config, scope: 'regular'},
+    function() {});
   }
   else{
       button.value = 1;
       button.textContent = "On";
+      body.style.backgroundColor= "green";
       chrome.storage.sync.set({'value':1,'text':"On"},function(){});
     chrome.proxy.settings.clear(
         {scope: 'regular'},
